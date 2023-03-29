8c3f08a11b4a4899a180d2c32bf2db7ef0c05235
Enol Casielles <enolcasielles@gmail.com>
2023-03-29 08:47:50 +0200

    commit

--- public/main.js
+++ public/main.js
@@ -1,11 +1,21 @@
 var value = 0;
 
+function init() {
+  if(localStorage.getItem('value')) {
+    value = localStorage.getItem('value')
+  }
+  else value = 0;
+  document.getElementById("value").innerText = '' + value;
+}
+
 function more() {
   value++;
   document.getElementById("value").innerText = '' + value;
+  localStorage.setItem('value', value);
 }
 
 function less() {
   value--;
   document.getElementById("value").innerText = '' + value;
+  localStorage.setItem('value', value);
 }
